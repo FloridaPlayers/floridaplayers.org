df9b441a2889d36fad920ccbd51a1346025c253a
Marcus Ball <Marcus Ball>
2013-09-20 20:56:46 -0400

    New resources page

--- www/svr/pages/page.resources.php
+++ www/svr/pages/page.resources.php
@@ -2,6 +2,7 @@
 
 class Page{
 	var $request = null;
+	var $sql = null;
 
 	function Page($request){
 		$this->request = $request;
@@ -15,30 +16,54 @@ class Page{
 	//Page specific content for the <head> section.
 	function customHead(){?>
 		<style type="text/css">
-			ul#resourceList{
+			ul.resourceList{
 				list-style: none outside none;
-				padding: 0;
+				padding: 5px 10px 10px;
 				margin: 10px 0;
+				border: 1px solid #DDDDDD;
+				color: #9A9A9A;
 			}
-			#resourceList li{
+			.resourceList li{
+				border-top: 1px solid #DDDDDD;
 				display: block;
-				width: 200px;
-				height: 45px;
-				background-color: #0e3eaa;
 				font-family: AurulentSansRegular;
-				font-size: 18px;
+				font-size: 15px;
 				font-weight: normal;
-				color: #fff;
-				text-overflow: ellipsis;
-				float: left;
-				margin: 5px;
+				margin: 5px 0 0;
+				padding: 7px 0 2px 35px;
 			}
-			#resourceList li a{
+			.resourceList li:first-of-type{
+				border-top: 0px solid #fff;
+				margin: 0;
+			}
+			.resourceList li.directory{
+				padding-left: 35px;
+				background: url('/res/images/layout/res-folder.png') no-repeat bottom left transparent;
+			}
+			.resourceList li.indir{
+				margin-left: 35px;
+			}
+			.resourceList li.type-none{
+				background: url('/res/images/layout/res-file.png') no-repeat bottom left transparent;
+			}
+			.resourceList li.type-pdf{
+				background: url('/res/images/layout/res-pdf.png') no-repeat bottom left transparent;
+			}
+			.resourceList li.type-doc{
+				background: url('/res/images/layout/res-doc.png') no-repeat bottom left transparent;
+			}
+			.resourceList li.type-zip{
+				background: url('/res/images/layout/res-zip.png') no-repeat bottom left transparent;
+			}
+			.resourceList li.type-link{
+				background: url('/res/images/layout/res-link.png') no-repeat bottom left transparent;
+			}
+			.resourceList li.type-globe{
+				background: url('/res/images/layout/res-globe.png') no-repeat bottom left transparent;
+			}
+			.resourceList li a{
 				display: block;
-				width: 180px;
-				height: 25px;
-				color: #fff;
-				padding: 10px;
+				color: #9A9A9A !important;
 			}
 		</style>
 			<!-- CUSTOM CSS/LINK HERE -->
@@ -51,6 +76,8 @@ class Page{
 	 * If it returns false, the page will stop loading.
 	 **/
 	function preloadPage(){
+		$this->sql = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
+		$this->sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return true; 
 	}
 
@@ -67,15 +94,52 @@ class Page{
 	//Return nothing; print out the page. 
 	function getContent(){
 		?>
+		<h1>Resources</h1>
 		<article>
-			<section>This will be more organized and nicer later.</section>
 			<section>
-				<ul id="resourceList">
-					<li><a href="/res/docs/constitution.pdf">Constitution</a></li>
-					<li><a href="/map">Maps</a></li>
-					<li><a href="/points">Points</a></li>
-					<li><a href="/res/docs/New-Works-Proposal-Guidelines-2013.pdf">Proposal Guidelines</a></li>
-					<li><a href="/res/docs/MLP-Matching-2013.pdf">Mentor/Mentee Form</a></li>
+				<h2>Links</h2>
+				<ul class='resourceList'>
+				<?php
+				$dir = '';
+				$query = 'SELECT * FROM resources WHERE type=\'link\' ORDER BY dir,modified';
+				foreach($this->sql->query($query) as $row){
+					$type = $row['icon'];
+					if($type == 'none'){ $type = 'link'; }
+					
+					if($row['dir'] != $dir){
+						$dir = $row['dir'];
+						printf('<li class="directory">%s</li>',$dir);
+					}
+					if($dir == ''){
+						printf('<li class="%s"><a href="%s">%s</a></li>','type-'.$type,$row['path'],$row['name']);
+					}
+					else{
+						printf('<li class="indir %s"><a href="%s">%s</a></li>','type-'.$type,$row['path'],$row['name']);
+					}
+				}
+				?>
+				</ul>
+				<div class="clear"></div>
+			</section>
+			<section>
+				<h2>Files</h2>
+				<ul class='resourceList'>
+				<?php
+				$dir = '';
+				$query = 'SELECT * FROM resources WHERE type=\'file\' ORDER BY dir,modified';
+				foreach($this->sql->query($query) as $row){
+					if($row['dir'] != $dir){
+						$dir = $row['dir'];
+						printf('<li class="directory">%s</li>',$dir);
+					}
+					if($dir == ''){
+						printf('<li class="%s"><a href="%s">%s</a></li>','type-'.$row['icon'],$row['path'],$row['name']);
+					}
+					else{
+						printf('<li class="indir %s"><a href="%s">%s</a></li>','type-'.$row['icon'],$row['path'],$row['name']);
+					}
+				}
+				?>
 				</ul>
 				<div class="clear"></div>
 			</section>
