8067d40a4c682f4702b12d3d589557d7e575bc9a
Marcus Ball <marcus@tapshield.com>
2013-10-19 23:49:45 -0400

    Commented out descriptions; left only images on contact page

--- www/svr/pages/page.about.contact.php
+++ www/svr/pages/page.about.contact.php
@@ -100,8 +100,8 @@ class Page{
 						<span class="name">Megan Wicks</span>
 						<span class="position">Artistic Director</span>
 						<span class="email"><?php echo $this->str_rot('artisticdirector@floridaplayers.org',17); ?></span>
-						<p>Megan is honored to be Artistic Director of Florida Players for the 2013-2014 school year. In the past two years, she has been involved with the executive board of FP as Secretary and Special Events coordinator and has acted in Children's Hour, Othello, Jesus Hopped the A Train, Iceland Play, Maple and Vine, and Mulier Ridiculum. She enjoys vegan food, the Insanity workout, and tea. Art is the portal to the soul, and Florida Player strives to provide opportunities to express yourself and your passion....contact me to get involved! 
-Contact for: General questions regarding Florida Players, Master Class</p>
+						<!-- <p>Megan is honored to be Artistic Director of Florida Players for the 2013-2014 school year. In the past two years, she has been involved with the executive board of FP as Secretary and Special Events coordinator and has acted in Children's Hour, Othello, Jesus Hopped the A Train, Iceland Play, Maple and Vine, and Mulier Ridiculum. She enjoys vegan food, the Insanity workout, and tea. Art is the portal to the soul, and Florida Player strives to provide opportunities to express yourself and your passion....contact me to get involved! 
+Contact for: General questions regarding Florida Players, Master Class</p> -->
 					</li>
 					<li>
 						<img src="/res/images/exec-board/kevin.jpg" />
@@ -114,7 +114,7 @@ Contact for: General questions regarding Florida Players, Master Class</p>
 						<span class="name">Kimberly Yeoman</span>
 						<span class="position">Production Manager</span>
 						<span class="email"><?php echo $this->str_rot('productionmanager@floridaplayers.org',17); ?></span>
-						<p>Kimberly Yeoman (BFA Scenic Design) is super excited to be on the Florida Players Board in her final year at UF. Previous Florida Players credits include Scenic Design for 'New Works Festival 2012' and 'Jeffrey,' and Props Design for 'Jesus Hopped the A Train.' 
+						<!-- <p>Kimberly Yeoman (BFA Scenic Design) is super excited to be on the Florida Players Board in her final year at UF. Previous Florida Players credits include Scenic Design for 'New Works Festival 2012' and 'Jeffrey,' and Props Design for 'Jesus Hopped the A Train.' 
 Kimberly is in charge of the shows that are produced within Florida Players, so please contact her with any show-related concerns or if you want to get involved with things like:
 Tech Crews
 Auditions
@@ -122,14 +122,14 @@ Design
 Stage Management
 House Management
 Show Proposals
-Etc.</p>
+Etc.</p> -->
 					</li>
 					<li>
 						<img src="/res/images/exec-board/katina.jpg" />
 						<span class="name">Katina White</span>
 						<span class="position">Treasurer</span>
 						<span class="email"><?php echo $this->str_rot('treasurer@floridaplayers.org',17); ?></span>
-						<p>Katina is enthralled to be your treasurer. Her first experience with the Florida Players was light board operator for Jeffrey. She enjoys kittens, long walks on the beach, managing the money, and creating SARs. Please contact her for any Florida Players financial concerns or if you are interested in joining the finance committee. </p>
+						<!-- <p>Katina is enthralled to be your treasurer. Her first experience with the Florida Players was light board operator for Jeffrey. She enjoys kittens, long walks on the beach, managing the money, and creating SARs. Please contact her for any Florida Players financial concerns or if you are interested in joining the finance committee. </p> -->
 					</li>
 					<li>
 						<img src="/res/images/exec-board/lauren.jpg" />
@@ -142,7 +142,7 @@ Etc.</p>
 						<span class="name">Christian Allison</span>
 						<span class="position">Assistant Publicist</span>
 						<span class="email"><?php echo $this->str_rot('asst.publicist@floridaplayers.org',17); ?></span>
-						<p>Christian is new to the executive board of Florida Players. In addition to graphic design, he enjoys theatre, music and more tea than can be considered healthy. As Assistant Publicist, he works primarily with the visual aspect of the production, creating posters, pamphlets, and anything else that can be used to promote the organization and its shows! Anyone with any interest in the Publicity team may contact him or the Senior Publicist, Lauren Killer.</p>
+						<!-- <p>Christian is new to the executive board of Florida Players. In addition to graphic design, he enjoys theatre, music and more tea than can be considered healthy. As Assistant Publicist, he works primarily with the visual aspect of the production, creating posters, pamphlets, and anything else that can be used to promote the organization and its shows! Anyone with any interest in the Publicity team may contact him or the Senior Publicist, Lauren Killer.</p> -->
 					</li>
 					<li>
 						<img src="/res/images/exec-board/drew.jpg" />
@@ -161,7 +161,7 @@ Etc.</p>
 						<span class="name">Anthony Bido</span>
 						<span class="position">Historian</span>
 						<span class="email"><?php echo $this->str_rot('historian@floridaplayers.org',17); ?></span>
-						<p>Anthony Bido is a junior BFA acting major. He's been a member of Florida Players for the past two years working mostly behind the scenes. He enjoys lights and shiny things. Contact him for archive information and photo calls.</p>
+						<!-- <p>Anthony Bido is a junior BFA acting major. He's been a member of Florida Players for the past two years working mostly behind the scenes. He enjoys lights and shiny things. Contact him for archive information and photo calls.</p> -->
 					</li>
 					<li>
 						<img src="/res/images/exec-board/webmaster.jpg" />
