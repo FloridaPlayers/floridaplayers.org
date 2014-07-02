a9a507f720126da5b0b13788f9aed09d30465dbf
Marcus Ball <marcus.ball@live.com>
2014-07-02 00:06:52 -0400

    reversed veronica and lauren as publicist and asst. publicist

--- www/svr/pages/page.contact.php
+++ www/svr/pages/page.contact.php
@@ -133,19 +133,19 @@ class Page{
 					</li>
 					<li>
 						<a name="pub"></a>
-						<img src="/res/images/exec-board/lauren.png" />
-						<span class="name">Lauren Elizabeth-Killer</span>
+						<img src="/res/images/exec-board/veronica.png" />
+						<span class="name">Veronica Michelle</span>
 						<span class="position">Publicist</span>
 						<span class="email"><?php echo $this->str_rot('publicist@floridaplayers.org',17); ?></span>
-						<p>Lauren Killer is a third year English and Theatre major, with a minor in mass communications. Essentially, she's crazy. This is her second year serving on the Florida Players executive board, and her third year working with this mah-velous organization. When not acting on the Florida Players stage or working behind the scenes, she is trying to be Tina Fey, doing a downward dog, or crafting her little heart out. Check out her live and online comedy show “Just Kidding,” coming to an interwebs near you.</p>
+						<p>Veronica is very excited to serve as Publicist for Florida Players during her last year at UF. She is studying both theatre and advertising, and has been a member of Florida Players since her freshman year. Spreading the word about this wonderful organization and all the opportunities it offers is a task she will do with pride. Contact her if interested in joining the Publicity team.</p>
 					</li>
 					<li>
 						<a name="apub"></a>
-						<img src="/res/images/exec-board/veronica.png" />
-						<span class="name">Veronica Michelle</span>
+						<img src="/res/images/exec-board/lauren.png" />
+						<span class="name">Lauren Elizabeth-Killer</span>
 						<span class="position">Assistant Publicist</span>
 						<span class="email"><?php echo $this->str_rot('asst.publicist@floridaplayers.org',17); ?></span>
-						<p>Veronica is very excited to serve as Publicist for Florida Players during her last year at UF. She is studying both theatre and advertising, and has been a member of Florida Players since her freshman year. Spreading the word about this wonderful organization and all the opportunities it offers is a task she will do with pride. Contact her if interested in joining the Publicity team.</p>
+						<p>Lauren Killer is a third year English and Theatre major, with a minor in mass communications. Essentially, she's crazy. This is her second year serving on the Florida Players executive board, and her third year working with this mah-velous organization. When not acting on the Florida Players stage or working behind the scenes, she is trying to be Tina Fey, doing a downward dog, or crafting her little heart out. Check out her live and online comedy show “Just Kidding,” coming to an interwebs near you.</p>
 					</li>
 					<li>
 						<a name="sec"></a>
